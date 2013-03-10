6560387e4caadbcc78fe0c3386bc88d4b4884c0c
Brian Yingling <brian_yingling@yahoo.com>
2013-03-10 18:53:56 -0400

    added delete functionality to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,24 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = @auth.posts.all
   end
   def new
     @post = Post.new
+    @klasses = @auth.courses.map {|course| course.klasses }
   end
   def create
     @post = Post.create(params[:post])
   end
   def show
+    binding.pry
   end
   def edit
   end
   def update
   end
   def destroy
+    post = Post.find(params[:id])
+    post.delete
+    redirect_to posts_path
   end
 end
