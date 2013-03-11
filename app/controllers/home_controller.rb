09154e6840dc3e2ffa0d9264f0d5b9236688ceb6
Brian Yingling <brian_yingling@yahoo.com>
2013-03-11 14:14:34 -0400

    users are routed to dashboard page if logged in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
     #redirect_to(@auth, :remote=>true) if @auth.present?
+    if @auth.present?
+      redirect_to @auth
+    end
   end
 end
