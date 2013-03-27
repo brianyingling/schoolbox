07fba4004d37b27d030e63735c30efd950efbd78
Brian Yingling <brian_yingling@yahoo.com>
2013-03-26 23:30:43 -0400

    added skeleton navbar for teachers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,31 @@ module ApplicationHelper
   def teacher_nav
     nav = ""
     if @auth.is_a?(Teacher)
-      nav += "<li><a>Courses</a></li>"
-      nav += "<li><a>Classes</a></li>"
-      nav += "<li><a>Posts</a></li>"
-      nav += "<li><a>Assignments</a></li>"
+      nav += "<li class='has-dropdown'>"
+        nav += "<a href='#'>Courses</a>"
+          nav += "<ul class='dropdown'>"
+          nav += "<li><a href='#' class=''>Add</a></li>"
+          nav += "<li><a href='#' class=''>Edit</a></li>"
+          nav += "<li><a href='#' class=''>View</a></li></ul>"
+      nav += "<li class='has-dropdown'>"
+        nav += "<a href='#'>Classes</a>"
+          nav += "<ul class='dropdown'>"
+          nav += "<li><a href='#' class=''>Add</a></li>"
+          nav += "<li><a href='#' class=''>Edit</a></li>"
+          nav += "<li><a href='#' class=''>View</a></li></ul>"
+      nav += "<li class='has-dropdown'>"
+        nav += "<a href='#'>Posts</a>"
+          nav += "<ul class='dropdown'>"
+          nav += "<li><a href='#' class=''>Add</a></li>"
+          nav += "<li><a href='#' class=''>Edit</a></li>"
+          nav += "<li><a href='#' class=''>View</a></li></ul>"
+      nav += "<li class='has-dropdown'>"
+        nav += "<a href='#'>Assignments</a>"
+          nav += "<ul class='dropdown'>"
+          nav += "<li><a href='#' class=''>Add</a></li>"
+          nav += "<li><a href='#' class=''>Edit</a></li>"
+          nav += "<li><a href='#' class=''>View</a></li></ul>"
+
     end
     nav
   end
