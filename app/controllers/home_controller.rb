06a913c9a8bdf8a8a91a1732037a7fc5911cac8b
Brian Yingling <brian_yingling@yahoo.com>
2013-04-01 00:36:32 -0400

    modified styling for dashboard teacher calendar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,9 +6,7 @@ class HomeController < ApplicationController
 
 
   def dashboard
-    # @student = @auth if @auth.is_a?(Student)
     @assignments = Assignment.all
-    binding.pry
     @assignments_by_date = @assignments.group_by(&:duedate)
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
 
